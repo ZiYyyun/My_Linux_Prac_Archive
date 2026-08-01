e720bfb0a6269b043633d5db5338fdff29c333c8
紫云 <1583931339@qq.com>
2026-08-01 16:48:14 +0800

    1

--- MyFstKernelModule.c
+++ MyFstKernelModule.c
@@ -2,20 +2,18 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
-static int __init hello_init(void)
-{
-    printk();
-    return 0;
+static int __init hello_init(void) {
+  printk();
+  return 0;
 }
 
-static void __exit hello_exit(void)
-{
-    printk(KERN_INFO "Hello Driver Exit!\n");
+static void __exit hello_exit(void) {
+  printk(KERN_INFO "Hello Driver Exit!\n");
 }
 
-module_init();
-module_exit();
+module_init(hello_init);
+module_exit(hello_exit);
 
-MODULE_LICENSE();
-MODULE_AUTHOR();
-MODULE_DECRIPTION();
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("ziyun");
+MODULE_DECRIPTION("My Fst Linux Kernel Module...");
