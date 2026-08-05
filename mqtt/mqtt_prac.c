ba58f185dde55cebf74b711520aa8b89084fdeaa
紫云 <1583931339@qq.com>
2026-08-05 20:59:00 +0800

    mqtt tiny exam

--- mqtt/mqtt_prac.c
+++ mqtt/mqtt_prac.c
@@ -11,3 +11,35 @@ static void TCP_Create()
     inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
 
 }
+
+void mqtt_main(void)
+{
+    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+    data.clientID.cstring = "gateway_client_01";
+    data.keepAliveInterval = 60;
+    data.cleansession = 1;
+
+    len = MQTTSerialize_connect(buf, sizeof(buf), &data);
+    send(sockfd, buf, len, 0);
+
+    //receive and chaijie CONNACK
+    rc = recv(sockfd, buf, 0);
+    if (rc <= 0) {
+        printf("did not receive Broker response...\n");
+        close(sockfd);
+        return 1;
+    }
+
+    // PUBLISH baowen
+    unsigned char dup = 0;
+    int qos = 0;
+    unsigned char retained = 0;
+    unsigned short packetid = 1;
+
+    char *payload = "hello from C gateway";
+    int payloadlen = strlen(payload);
+
+    len = MQTTSerialize_publish(buf, sizeof(buf), dup, qos, retained, packetid, topicString);
+
+
+}
